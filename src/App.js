737028701af30cbbe136840ab4ce90883c28cc25
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-09-04 16:45:09 +0530

    created tracks component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        < Navbar />
-      </div>
+      <Router>
+        <React.Fragment>
+          < Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
