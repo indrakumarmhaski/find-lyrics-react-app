35ff216a23f3aeda8f32f53f7ada570a7ff4da7c
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-09-05 10:56:18 +0530

    Configured app for global state, using context api

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
-import Index from './components/layout/Index';
-
+import Index from './components/layout/Index'; 
+import { Provider } from './context';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          < Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            < Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
