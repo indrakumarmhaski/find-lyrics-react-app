85371711a8b94e929ab6a4a35e751e7da62275f7
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-09-06 12:59:21 +0530

    Configured Lyrics.js component to show initial lyrics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index'; 
 import { Provider } from './context';
+import Lyrics from './components/tracks/Lyrics';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
